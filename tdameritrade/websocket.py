aa389cd7e9332f3f21ec0a847b1b50962c507805
Ryan Smith <smith.a.ryan.98@gmail.com>
2022-04-17 02:22:23 -0400

    refactor: message creation and sending

---
+++ tdameritrade/websocket.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+##-------------------------------##
+## TDAmeritrade PyAPI            ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+## TDAmeritrade Websocket        ##
+##-------------------------------##
+
+## Imports
+from datetime import datetime
+from urllib.parse import urlencode
+
+import aiohttp
+
+from .typing import RequestDict
+
+
+## Classes
+class ClientWebSocket(aiohttp.ClientWebSocketResponse):
+    """TDAmeritrade Client WebSocket"""
+
+    # -Constructor
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.id: str | None = None
+        self.source: int | None = None
+        self.request_counter: int = 0
+
+    # -Instance Methods: Public Primary
+    async def login(
+        self, id_: str, source_id: int, token: str,
+        company: str, segment: str, domain: str, user_group: str, access_level: str,
+        dt: datetime, acl: str, qos: int = 2
+    ) -> None:
+        '''Login request'''
+        self.id = id_
+        self.source = source_id
+        msg: RequestDict = self.create_message(
+            "ADMIN", "LOGIN", token=token, version="1.0",
+            qoslevel=qos, credential=urlencode({
+                'accesslevel': access_level,
+                'acl': acl,
+                'appid': id_,
+                'authorized': "Y",
+                'cddomain': domain,
+                'company': company,
+                'segment': segment,
+                'timestamp': int(dt.timestamp() * 1000),
+                'token': token,
+                'usergroup': user_group,
+                'userid': source_id,
+            })
+        )
+        await self.send_messages(msg)
+
+    async def logout(self) -> None:
+        '''Logout request'''
+        msg: RequestDict = self.create_message("ADMIN", "LOGOUT")
+        await self.send_messages(msg)
+
+    async def quality_of_service(self, qos: int) -> None:
+        '''Change data rate speed request'''
+        msg: RequestDict = self.create_message("ADMIN", "QOS", qoslevel=qos)
+        await self.send_messages(msg)
+
+    # -Instance Methods: Public Secondary
+    def create_message(self, service: str, command: str, **kwargs):
+        '''Create a formatted message request for sending'''
+        request: RequestDict = {
+            'account': self.id,
+            'command': command,
+            'parameters': kwargs if kwargs else {},
+            'requestid': self.request_counter,
+            'service': service,
+            'source': self.source,
+        }
+        self.request_counter += 1
+        return request
+
+    async def send_messages(self, requests: RequestDict | list[RequestDict]) -> None:
+        '''Send formatted message request or list of requests'''
+        if not isinstance(requests, list):
+            requests = [requests]
+        await self.send_json({'requests': requests})
