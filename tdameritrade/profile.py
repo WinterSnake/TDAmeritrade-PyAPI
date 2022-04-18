7b27c3a62a1153af4dcf8d747f450365c9ab5e8e
Ryan Smith <smith.a.ryan.98@gmail.com>
2022-04-18 12:49:45 -0400

    add: user principal method

--- tdameritrade/profile.py
+++ tdameritrade/profile.py
@@ -7,8 +7,9 @@
 ##-------------------------------##
 
 ## Imports
-from .session import ClientSession
-from .websocket import ClientWebSocket
+from .. import urls
+from ..session import ClientSession
+from ..websocket import ClientWebSocket
 
 
 ## Classes
@@ -18,3 +19,21 @@ class Profile:
     # -Constructor
     def __init__(self, session: ClientSession) -> None:
         self._session: ClientSession = session
+
+    # -Instance Methods
+    async def user_principals(
+        self, preferences: bool = False, surrogate_ids: bool = False,
+        streamer_keys: bool = False, streamer_info: bool = False,
+    ) -> None:
+        '''Get user principals and additional information for profile and accounts'''
+        # -Populate fields
+        fields: list[str] = []
+        if preferences:
+            fields.append("preferences")
+        if surrogate_ids:
+            fields.append("surrogateIds")
+        if streamer_keys:
+            fields.append("streamerSubscriptionKeys")
+        if streamer_info:
+            fields.append("streamerConnectionInfo")
+        return await self._session.get(urls.v1.user_principals(), params={'fields': fields})
